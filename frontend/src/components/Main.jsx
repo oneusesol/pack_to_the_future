b75bf34d554f8a4a9c5beaa70bfa1c766fd6229e
김효진 <dlapdlffldpald1@naver.com>
2025-04-22 23:39:10 +0900

    structure_1

---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,11 @@
+
+function Main() {
+  return (
+    <div>
+      <h1>Welcome to the Main Component</h1>
+      <p>This is the main content area.</p>
+    </div>
+  );
+}
+
+export default Main;
---
+++ frontend/src/pages/Splash.jsx
@@ -0,0 +1,10 @@
+
+function Splash() {
+  return (
+     <div>
+        <h3>Splash</h3>
+    </div>
+  )
+}
+
+export default Splash
