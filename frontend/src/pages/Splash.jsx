04e72cb6d2bcbd3dd04f0989f51964a2ea8af009
김효진 <dlapdlffldpald1@naver.com>
2025-04-23 02:31:41 +0900

    install Tailwindcss

--- frontend/src/pages/Splash.jsx
+++ frontend/src/pages/Splash.jsx
@@ -3,7 +3,8 @@ import logoImg from '../assets/Splash_logo.png'
 
 function Splash() {
   return (
-     <div>
+     <div className = "bg-green-300 text-center text-xl p-4">
+      Tailwind 스타일 적용 확인
         <img src = {logoImg} alt = 'splash' />
     </div>
   )
